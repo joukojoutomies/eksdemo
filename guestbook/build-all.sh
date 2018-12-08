84d83b1edbee364fc3e986308862e4497a7fabfb
root <root@ip-172-18-1-223.ec2.internal>
2018-12-08 15:57:04 -0500

    jouni

---
+++ guestbook/build-all.sh
@@ -0,0 +1,7 @@
+## Building the Docker images
+docker build -t docker.io/lindstedt/gb-frontend:v5 php-redis
+docker build -t docker.io/lindstedt/gb-redisslave:v2 redis-slave
+make -C php-redis
+make -C redis-slave
+make -C php-redis all-push
+make -C redis-slave all-push
